fd897273cc272a74113f66984ba0aa03fcd0aa3e
JaneSeongHo99 <qq9725@ajou.ac.kr>
2020-12-06 06:52:43 +0000

    Modify logic, finding detail by url-id

--- src/Detail.js
+++ src/Detail.js
@@ -5,18 +5,27 @@ function Detail(props){
 	
 	let { id } = useParams();
 	let history = useHistory();
+	let match = props.shoes.find(function(shoe){
+		return shoe.id == id
+	})
 	
+	for(let shoe of props.shoes) {
+		if (shoe.id == id) {
+			match = shoe.id;
+			break;
+		}
+	}
 	return(
 		<div className="container">
 			<div className="row">
 				<div className="col-md-6">
-					<img src={"https://codingapple1.github.io/shop/shoes"+(Number(id)+1)+".jpg"} width="100%"/>
+					<img src={"https://codingapple1.github.io/shop/shoes"+(match+1)+".jpg"} width="100%"/>
 				</div>
 				<div className="col-md-6 mt-4">
-					<h4 className="pt-5">{props.shoes[id].title}</h4>
-					<p>{props.shoes[id].content}</p>
-					<p>{props.shoes[id].price}</p>
-					<button className="btn btn-danger">주문하기</button>
+					<h4 className="pt-5">{props.shoes[match].title}</h4>
+					<p>{props.shoes[match].content}</p>
+					<p>{props.shoes[match].price}</p>
+					<button className="btn btn-danger" onClick={()=>{console.log(match)}}>주문하기</button>
 					<button className="btn btn-danger" onClick={()=>{
 						history.goBack();	
 						
